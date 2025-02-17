7a01b44c5506ffbb2a2f9d2bdec4be3704529b58
Tintin <oofedindeed@gmail.com>
2025-02-16 22:05:11 -0500

    Reverted to previous commit

--- models/Task.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const taskSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true },
-    timeLogs: [{ date: String, timeSpent: Number }],
-});
-
-export default mongoose.models.Task || mongoose.model('Task', taskSchema);
